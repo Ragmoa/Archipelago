d4837a5d0850471ac8d65d922c4d9871da142418
Ragmoa <ryckaert.guillaume@neuf.fr>
2026-08-21 13:47:14 +0200

    Fix entrances & disable Hourglass orbs rando

--- worlds/okamihd/RegionsData/r310.py
+++ worlds/okamihd/RegionsData/r310.py
@@ -22,12 +22,12 @@ exits = {
                  required_items_events=["Yoshpet - First section - Pass high vine wall"]),
     ],
     RegionNames.YOSHPET_1_2: [
-        ExitData(RegionNames.YOSHPET_2_1, one_way=True),
+        ExitData(RegionNames.YOSHPET_2_1),
         # When running out of time
         ExitData(RegionNames.YOSHPET_ENTRANCE, one_way=True)
     ],
     RegionNames.YOSHPET_2_1: [
-        ExitData(RegionNames.YOSHPET_3_1, one_way=True),
+        ExitData(RegionNames.YOSHPET_3_1),
         # When running out of time
         ExitData(RegionNames.YOSHPET_ENTRANCE, one_way=True)
     ],
@@ -52,12 +52,12 @@ locations = {
     RegionNames.YOSHPET_1_1: {
         "Yoshpet - First section - Freestanding chest after vine wall at entrance": LocData(
             container_check_id(MapIds.YOSHPET, 16)),
-        "Yoshpet - First section - Hourglass orb after vine walls at cliff's bottom": LocData(
-            container_check_id(MapIds.YOSHPET, 0), type=LocationType.HOURGLASS_ORB),
+        #"Yoshpet - First section - Hourglass orb after vine walls at cliff's bottom": LocData(
+        #    container_check_id(MapIds.YOSHPET, 0), type=LocationType.HOURGLASS_ORB),
         "Yoshpet - First section - Freestanding chest near cliff's top": LocData(
             container_check_id(MapIds.YOSHPET, 12)),
-        "Yoshpet - First section - Hourglass orb on Frozen Lake Platforms": LocData(
-            container_check_id(MapIds.YOSHPET, 13), type=LocationType.HOURGLASS_ORB),
+       # "Yoshpet - First section - Hourglass orb on Frozen Lake Platforms": LocData(
+       #     container_check_id(MapIds.YOSHPET, 13), type=LocationType.HOURGLASS_ORB),
         "Yoshpet - First section - Freestanding chest on Frozen Lake Platforms": LocData(
             container_check_id(MapIds.YOSHPET, 1)),
 
@@ -68,26 +68,26 @@ locations = {
     },
     RegionNames.YOSHPET_2_1: {
         "Yoshpet - Second section - Freestanding Chest after entrance": LocData(container_check_id(MapIds.YOSHPET, 3)),
-        "Yoshpet - Second section - Hourglass orb before first vine wall": LocData(
-            container_check_id(MapIds.YOSHPET, 4), type=LocationType.HOURGLASS_ORB),
+        #"Yoshpet - Second section - Hourglass orb before first vine wall": LocData(
+        #    container_check_id(MapIds.YOSHPET, 4), type=LocationType.HOURGLASS_ORB),
         "Yoshpet - Second section - Freestanding Chest between second and third vine wall": LocData(
             container_check_id(MapIds.YOSHPET, 5)),
         "Yoshpet - Second section - Freestanding Chest on ice platform in poison lake": LocData(
             container_check_id(MapIds.YOSHPET, 17)),
-        "Yoshpet - Second section -  Hourglass orb on ice platform in poison lake": LocData(
-            container_check_id(MapIds.YOSHPET, 6), type=LocationType.HOURGLASS_ORB),
+       # "Yoshpet - Second section -  Hourglass orb on ice platform in poison lake": LocData(
+       #     container_check_id(MapIds.YOSHPET, 6), type=LocationType.HOURGLASS_ORB),
         "Yoshpet - Second section -  Freestanding chest before final fork": LocData(
             container_check_id(MapIds.YOSHPET, 7)),
         "Yoshpet - Second section - Freestanding chest at right end of final fork": LocData(
             container_check_id(MapIds.YOSHPET, 8)),
     },
     RegionNames.YOSHPET_3_1: {
-        "Yoshpet - Third section - Hourglass orb on ice lake after snow balls": LocData(
-            container_check_id(MapIds.YOSHPET, 14), type=LocationType.HOURGLASS_ORB),
+        #"Yoshpet - Third section - Hourglass orb on ice lake after snow balls": LocData(
+        #    container_check_id(MapIds.YOSHPET, 14), type=LocationType.HOURGLASS_ORB),
         "Yoshpet - Third section - Freestanding chest on ice lake after snow balls": LocData(
             container_check_id(MapIds.YOSHPET, 15)),
-        "Yoshpet - Third section - Hourglass orb after ice lake": LocData(
-            container_check_id(MapIds.YOSHPET, 9), type=LocationType.HOURGLASS_ORB),
+       #"Yoshpet - Third section - Hourglass orb after ice lake": LocData(
+       #    container_check_id(MapIds.YOSHPET, 9), type=LocationType.HOURGLASS_ORB),
         "Yoshpet - Third section - Freestanding chest on hill's right side, after ice lake": LocData(
             container_check_id(MapIds.YOSHPET, 10)),
         # Not marking this one as buried since you can't use Crescent in Yoshpet. So this chest should never require it logically
